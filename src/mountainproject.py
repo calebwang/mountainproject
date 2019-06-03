108bd758871a399d8b04dd749a0588f4176c5f91
Caleb Wang <caleb@caleb.wang>
2019-06-02 18:51:32 -0700

    Add Route class

--- src/mountainproject.py
+++ src/mountainproject.py
@@ -2,6 +2,7 @@ import requests
 import json
 import itertools
 import os
+import re
 
 import util
 
@@ -51,6 +52,55 @@ class RouteCache(Cache):
     self.cache = {
       int(id): route for id, route in self.cache.items()
     }
+
+class Route(object):
+  def __init__(self, data):
+    self.data = data
+
+  def id(self):
+    return self.data["id"]
+
+  def name(self):
+    return self.data["name"]
+
+  def type(self):
+    # Prefer types in following order to resolve to a single type
+    # Trad > Sport > Boulder > TR
+    ranking = ["Trad", "Sport", "Boulder", "TR"]
+    types = [
+      t.strip() for t in
+      self.data["type"].split(",")
+    ]
+    return sorted(
+      types,
+      key=lambda t: ranking.index(t)
+    )[0]
+
+  def _extract_grade(self, regex):
+    grades = self.data["rating"].split(" ")
+    matching_grades = [
+      m.group(0) for m in [
+        re.match(regex, grade) for grade in grades
+      ] if m is not None
+    ]
+    if matching_grades:
+      return matching_grades[0]
+    return None
+
+  def _yds_grade(self):
+    return self._extract_grade(r"5\.[0-9]+[abcd+-]?")
+
+  def _v_grade(self):
+    return self._extract_grade(r"V[0-9]+")
+
+  def pg_grade(self):
+    return self._extract_grade(r"X|R|PG-13")
+
+  def grade(self):
+    if self.type() == "Boulder":
+      return self._v_grade()
+    else:
+      return self._yds_grade()
 
 class Api(object):
   BASE_URL = "https://www.mountainproject.com/data"
@@ -115,5 +165,8 @@ class Api(object):
 
   def routes(self, route_ids):
     request_limit=100
-    return util.map_chunk(route_ids, request_limit, self._routes_with_caching)
+    return [
+      Route(r) for r in 
+      util.map_chunk(route_ids, request_limit, self._routes_with_caching)
+    ]
     
