2a3e4a07b58ec8deb9f4df91850d74d80a8241df
Caleb Wang <caleb@caleb.wang>
2019-11-29 15:56:57 -0800

    Add PyPI setup files

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'mountainproject',
+  packages = ['mountainproject'],   # Chose the same as "name"
+  version = '1.0.0',      
+  license='MIT',        
+  description = 'MountainProject API Python wrapper',
+  author = 'Caleb Wang',                   # Type in your name
+  author_email = 'caleb@caleb.wang',
+  url = 'https://github.com/calebwang/mountainproject',
+  download_url = 'https://github.com/calebwang/mountainproject/archive/v1.0.0.tar.gz',
+  keywords = ['mountainproject'],
+  install_requires=[            # I get to this in a second
+    'requests',
+  ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
